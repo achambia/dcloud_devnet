8082110b1d7dfb218daafba783661cac3125c6cb
achambia <104783812+achambia@users.noreply.github.com>
2023-12-05 18:21:41 +1100

    Final Commit

--- Policy_deletion.py
+++ Policy_deletion.py
@@ -6,23 +6,23 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 import time
 
 Auth = Authentication()
-jsessionid = Auth.get_jsessionid('198.18.133.200', '8443', 'admin', 'C1sco12345')
-token = Auth.get_token('198.18.133.200', '8443', jsessionid)
+jsessionid = Auth.get_jsessionid('vManage_IP', 'vManage_Port', 'vManage_User', 'vManage_Password')
+token = Auth.get_token('vManage_IP', 'vManage_Port', jsessionid)
 if token is not None:
     header = {'Content-Type': "application/json", 'Cookie': jsessionid, 'X-XSRF-TOKEN': token}
 else:
     header = {'Content-Type': "application/json", 'Cookie': jsessionid}
 
 def policy_delete(policy_name,dns_policy):
-    response_global = requests.get(f'https://198.18.133.200:8443/dataservice/template/policy/security/',
+    response_global = requests.get(f'https://vManage_IP:vManage_Port/dataservice/template/policy/security/',
                                     headers=header, verify=False)
-    resp_dns_sec = requests.get(f'https://198.18.133.200:8443/dataservice/template/policy/definition/dnssecurity',
+    resp_dns_sec = requests.get(f'https://vManage_IP:vManage_Port/dataservice/template/policy/definition/dnssecurity',
                                     headers=header, verify=False)
     for x in response_global.json()['data']:
         if x['policyName'] == policy_name:
             if x['mastersAttached'] == 0:
                 response_global_delete = requests.delete(
-                    f'https://198.18.133.200:8443/dataservice/template/policy/security/{x["policyId"]}',
+                    f'https://vManage_IP:vManage_Port/dataservice/template/policy/security/{x["policyId"]}',
                     headers=header, verify=False)
                 if response_global_delete.status_code == 200:
                     print("!! POLICY DELETED SUCCESSFULLY !!")
@@ -33,7 +33,7 @@ def policy_delete(policy_name,dns_policy):
     for y in resp_dns_sec.json()['data']:
         if y['name'] == dns_policy:
             resp_dns = requests.delete(
-                f'https://198.18.133.200:8443/dataservice/template/policy/definition/dnssecurity/{y["definitionId"]}',
+                f'https://vManage_IP:vManage_Port/dataservice/template/policy/definition/dnssecurity/{y["definitionId"]}',
                 headers=header, verify=False)
 
             if resp_dns.status_code == 200:

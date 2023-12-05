354e7f2e79b7756139767ec200e1b6ff8d7b5ccc
achambia <104783812+achambia@users.noreply.github.com>
2023-12-05 18:27:45 +1100

    Final Commit

--- Umb_DNS_Only_Policy.py
+++ Umb_DNS_Only_Policy.py
@@ -6,8 +6,8 @@ import sys
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 Auth = Authentication()
-jsessionid = Auth.get_jsessionid('198.18.133.200', '8443', 'admin', 'C1sco12345')
-token = Auth.get_token('198.18.133.200', '8443', jsessionid)
+jsessionid = Auth.get_jsessionid('vManage_IP', 'vManage_Port', 'vManage_User', 'vManage_Password')
+token = Auth.get_token('vManage_IP', 'vManage_Port', jsessionid)
 if token is not None:
     header = {'Content-Type': "application/json", 'Cookie': jsessionid, 'X-XSRF-TOKEN': token}
 else:
@@ -15,13 +15,13 @@ else:
 
 def dns_policy(dns_policy_name,dns_policy_des,match_vpn):
     #Getting Umbrella Credentials from Vmanage
-    response_umb_id = requests.get(f'https://198.18.133.200:8443/dataservice/template/policy/list/umbrelladata/',
+    response_umb_id = requests.get(f'https://vManage_IP:vManage_Port/dataservice/template/policy/list/umbrelladata/',
                                    headers=header, verify=False)
     payload = dict({"name": f"{dns_policy_name}", "type": "DNSSecurity", "description": f"{dns_policy_des}","definition": {"localDomainBypassList": {}, "matchAllVpn": False,
                     "targetVpns": [{"vpns": match_vpn, "umbrellaDefault": True, "localDomainBypassEnabled": True}],
                     "dnsCrypt": False, "umbrellaData": {"ref": f"{response_umb_id.json()['data'][0]['listId']}"}}})
     #Creating DNS Security Policy
-    response_global = requests.post(f'https://198.18.133.200:8443/dataservice/template/policy/definition/dnssecurity',
+    response_global = requests.post(f'https://vManage_IP:vManage_Port/dataservice/template/policy/definition/dnssecurity',
                                    headers=header, verify=False, json=(payload))
     if response_global.status_code != 200:
         if "Duplicate policy detected with name" in response_global.json()['error']['details']:
@@ -40,7 +40,7 @@ def dns_local_policy(dns_policy_name,dns_policy_des,dns_policy):
     payload = {"policyDescription":f"{dns_policy_des}","policyType":"feature","policyName":f"{dns_policy_name}","policyUseCase":"custom","policyMode":"unified","policyDefinition":{"assembly":[{"definitionId":f"{dns_policy}","type":"DNSSecurity"}],"settings":{}},"isPolicyActivated":False}
     #Creating the Security Policy with DNS Profile Attached
     print()
-    response_global = requests.post(f'https://198.18.133.200:8443/dataservice/template/policy/security/',
+    response_global = requests.post(f'https://vManage_IP:vManage_Port/dataservice/template/policy/security/',
                                     headers=header, verify=False, json=(payload))
     if response_global.status_code !=200:
         if "Security policy with name" in response_global.json()['error']['details']:
@@ -53,7 +53,3 @@ def dns_local_policy(dns_policy_name,dns_policy_des,dns_policy):
         print(response_global.text)
 
 
-
-#vpn =["10","30"]
-
-#dns_local_policy('abhiu','abhi',dns_policy('abhi','abhi',vpn))

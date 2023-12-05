c95e006f0ca3ebcd01eea3659646b06aa9d3fec0
achambia <104783812+achambia@users.noreply.github.com>
2023-12-05 18:29:41 +1100

    Final Commit

--- Umbrella_DNS_Credentials.py
+++ Umbrella_DNS_Credentials.py
@@ -8,22 +8,22 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def dns_creds(apikey,secret,org):
     Auth = Authentication()
-    jsessionid = Auth.get_jsessionid('198.18.133.200', '8443', 'admin', 'C1sco12345')
-    token = Auth.get_token('198.18.133.200', '8443', jsessionid)
+    jsessionid = Auth.get_jsessionid('vManage_IP', 'vManage_Port', 'vManage_User', 'vManage_Password')
+    token = Auth.get_token('vManage_IP', 'vManage_Port', jsessionid)
     if token is not None:
         header = {'Content-Type': "application/json", 'Cookie': jsessionid, 'X-XSRF-TOKEN': token}
     else:
         header = {'Content-Type': "application/json", 'Cookie': jsessionid}
     payload = {"name":"umbrellaTokenList","description":"","type":"umbrellaData","entries":[{"apiKey":apikey,"secret":secret,"umbOrgId":org,"token":""}]}
     #Sending Query to update the Umbrella Keys
-    response_global = requests.post(f'https://198.18.133.200:8443/dataservice/template/policy/list/umbrelladata/',
+    response_global = requests.post(f'https://vManage_IP:vManage_Port/dataservice/template/policy/list/umbrelladata/',
                                    headers=header, verify=False, data=json.dumps(payload))
     if (response_global.status_code) == 200:
         print("!! UMBRELLA DNS CREDENTIALS CREATED !!\n")
     elif(response_global.json()['error']['details'] == 'Umbrella data entry already exists'):
-        response_umb_id = requests.get(f'https://198.18.133.200:8443/dataservice/template/policy/list/umbrelladata/',
+        response_umb_id = requests.get(f'https://vManage_IP:vManage_Port/dataservice/template/policy/list/umbrelladata/',
                                         headers=header, verify=False)
-        response_umb = requests.put(f'https://198.18.133.200:8443/dataservice/template/policy/list/umbrelladata/{response_umb_id.json()["data"][0]["listId"]}',
+        response_umb = requests.put(f'https://vManage_IP:vManage_Port/dataservice/template/policy/list/umbrelladata/{response_umb_id.json()["data"][0]["listId"]}',
                                         headers=header, verify=False, data=json.dumps(payload))
         if response_umb.status_code == 200:
             print("!! UMBRELLA DNS CREDENTIALS UPDATED !! \n")

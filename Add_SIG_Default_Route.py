cc835f0d6a61a9e709032e281ab2fe54692d5d3b
achambia <104783812+achambia@users.noreply.github.com>
2023-12-05 14:44:04 +1100

    Final Commit

--- Add_SIG_Default_Route.py
+++ Add_SIG_Default_Route.py
@@ -6,8 +6,8 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 import time
 
 Auth = Authentication()
-jsessionid = Auth.get_jsessionid('198.18.133.200', '8443', 'admin', 'C1sco12345')
-token = Auth.get_token('198.18.133.200', '8443', jsessionid)
+jsessionid = Auth.get_jsessionid('vManage_IP', 'vManage_Port', 'vManage_user', 'vManage_Password')
+token = Auth.get_token('vManage_IP', 'vManage_Port', jsessionid)
 if token is not None:
     header = {'Content-Type': "application/json", 'Cookie': jsessionid, 'X-XSRF-TOKEN': token}
 else:
@@ -16,7 +16,7 @@ else:
 def default_route(temp_name,vpn,attached_devices):
     devic_id = []
     # Getting device Template information
-    response_global = requests.get(f'https://198.18.133.200:8443/dataservice/template/device',
+    response_global = requests.get(f'https://vManage_IP:vManage_Port/dataservice/template/device',
                                    headers=header, verify=False)
     for x in (response_global.json()['data']):
         if x['templateName'] in temp_name:
@@ -24,7 +24,7 @@ def default_route(temp_name,vpn,attached_devices):
     for y in devic_id:
         # Running Get to find all feature templates assigned to the Device Template
         print("!! RETREIVING THE FEATURE TEMPLATES FROM THE DEVICE TEMPLATE !! \n")
-        response_global_device = requests.get(f'https://198.18.133.200:8443/dataservice/template/device/object/{y}',
+        response_global_device = requests.get(f'https://vManage_IP:vManage_Port/dataservice/template/device/object/{y}',
                                               headers=header,
                                               verify=False, )
         device_temp = response_global_device.json()
@@ -34,7 +34,7 @@ def default_route(temp_name,vpn,attached_devices):
         for z in device_temp['generalTemplates']:
             if z['templateType'] == "cisco_vpn":
                 response_local = requests.get(
-                    f'https://198.18.133.200:8443/dataservice/template/feature/object/{z["templateId"]}',
+                    f'https://vManage_IP:vManage_Port/dataservice/template/feature/object/{z["templateId"]}',
                     headers=header,
                     verify=False, )
                 #Add service SIG Route to each VRF
@@ -44,13 +44,13 @@ def default_route(temp_name,vpn,attached_devices):
                     if response_local.json()['editedTemplateDefinition']['vpn-id']['vipValue'] == int(vrf):
                         print(f"!! Adding SIG Route to VPN {vrf}!!\n")
                         response_local_vpn = requests.get(
-                            f'https://198.18.133.200:8443/dataservice/template/feature/object/{z["templateId"]}',
+                            f'https://vManage_IP:vManage_Port/dataservice/template/feature/object/{z["templateId"]}',
                             headers=header,
                             verify=False, )
                         update_payload = (response_local_vpn.json())
                         update_payload['templateDefinition']['ip'].update({"service-route":{"vipType":"constant","vipValue":[{"prefix":{"vipObjectType":"object","vipType":"constant","vipValue":"0.0.0.0/0","vipVariableName":"vpn_serviceroute_prefix"},"vpn":{"vipObjectType":"object","vipType":"constant","vipValue":0},"service":{"vipObjectType":"object","vipType":"notIgnore","vipValue":"sig"},"priority-order":["prefix","vpn","service"]}],"vipObjectType":"tree","vipPrimaryKey":["prefix"]}})
                         response_local_dr = requests.put(
-                            f'https://198.18.133.200:8443/dataservice/template/feature/{z["templateId"]}',
+                            f'https://vManage_IP:vManage_Port/dataservice/template/feature/{z["templateId"]}',
                             headers=header,
                             verify=False, json=update_payload)
                         input_payload = {"templateId": y, "deviceIds": attached_devices, "isEdited": True,
@@ -58,7 +58,7 @@ def default_route(temp_name,vpn,attached_devices):
                         # Updating Variables and retreiving the payload
                         print("!! UPDATING PAYLOAD !! \n")
                         response_global_input = requests.post(
-                            f'https://198.18.133.200:8443/dataservice/template/device/config/input/',
+                            f'https://vManage_IP:vManage_Port/dataservice/template/device/config/input/',
                             headers=header, verify=False, json=input_payload)
                         attach_pay = response_global_input.json()['data']
                         for m in attach_pay:
@@ -67,12 +67,12 @@ def default_route(temp_name,vpn,attached_devices):
                             {"templateId": y, "device": attach_pay, "isEdited": True, "isMasterEdited": True}]}
                         # Asking Vmanage to Push the Config
                         response_global_attach = requests.post(
-                            f'https://198.18.133.200:8443/dataservice/template/device/config/attachfeature',
+                            f'https://vManage_IP:vManage_Port/dataservice/template/device/config/attachfeature',
                             headers=header, verify=False, json=global_attach_payload)
                         while push_verification != 'done':
                             # Verifying the Status of the task
                             done_verification = requests.get(
-                                f'https://198.18.133.200:8443/dataservice/device/action/status/{response_global_attach.json()["id"]}',
+                                f'https://vManage_IP:vManage_Port/dataservice/device/action/status/{response_global_attach.json()["id"]}',
                                 headers=header, verify=False, json=response_global_input.json())
                             push_verification = done_verification.json()['summary']['status']
                             print("!! VMANAGE PUSHING THE CONFIG !!! SLEEPING FOR 5 SECS\n")
@@ -83,10 +83,3 @@ def default_route(temp_name,vpn,attached_devices):
                                 f"!!! {done_verification.json()['summary']['count']['Failure']} devices Failed  to update!!!")
                         else:
                             print("!! SUCCESS !!! \n")
-
-
-
-
-#default_route('Branch_Dev_Temp_Site_400',[10,11])
-
-
